b641a02d7762f76e6dccc94a1d24645a931bd5d4
AndreiVIlla <andreivilla63@gmaIL.com>
2021-08-15 09:34:51 -0300

    arvores

--- P1/ex6.c
+++ P1/ex6.c
@@ -4,6 +4,7 @@
 int min(int a, int b, int c);
 int levenshtein(char *s1, char *s2);
 int len(char *str);
+int **alocaMat(int lin, int col);
 
 int main(int argc, char *agrv[]){
     char str1[100], str2[100];
@@ -11,10 +12,17 @@ int main(int argc, char *agrv[]){
     scanf("%s", str1);
     scanf("%s", str2);
 
-    //printf("%d", levenshtein("andrei", "nandretahu"));
-
     printf("%d", levenshtein(str1, str2));
     return 1;
+}
+
+int **alocaMat(int lin, int col){
+    int **mat;
+    int i;
+    mat = calloc(lin, sizeof(int*));
+    for(i=0; i<lin; i++)
+        mat[i] = calloc(col, sizeof(int));
+    return mat;
 }
 
 int min(int a, int b, int c){
@@ -36,22 +44,28 @@ int len(char *str){
     return count;
 }
 
-int levenshtein(char *s1, char *s2) {
-    int x, y, s1tam, s2tam;    
-    s1tam = len(s1);
-    s2tam = len(s2);
-    int mat[s2tam+1][s1tam+1];
-    mat[0][0] = 0;
+int levenshtein(char *str1, char *str2) {
+    int i, j, s1tam, s2tam;
+    int verif;
+    s1tam = len(str1);
+    s2tam = len(str2);
+    int **mat = alocaMat(s2tam+1, s1tam+1);
 
-    for (x = 1; x <= s2tam; x++)
-        mat[x][0] = mat[x-1][0] + 1;
+    for (i = 1; i <= s2tam; i++)
+        mat[i][0] = mat[i-1][0] + 1;
 
-    for (y = 1; y <= s1tam; y++)
-        mat[0][y] = mat[0][y-1] + 1;
+    for (i = 1; i <= s1tam; i++)
+        mat[0][i] = mat[0][i-1] + 1;
 
-    for (x = 1; x <= s2tam; x++)
-        for (y = 1; y <= s1tam; y++)
-            mat[x][y] = min(mat[x-1][y] + 1, mat[x][y-1] + 1, mat[x-1][y-1] + (s1[y-1] == s2[x-1] ? 0 : 1));
+    for (i = 1; i <= s2tam; i++)
+        for (j = 1; j <= s1tam; j++){
+            if(str1[j-1] == str2[i-1]){
+                verif = 0;
+            }else{
+                verif = 1;
+            }
+            mat[i][j] = min(mat[i-1][j] + 1, mat[i][j-1] + 1, mat[i-1][j-1] + verif);
+        }    
 
     return(mat[s2tam][s1tam]);
 }
